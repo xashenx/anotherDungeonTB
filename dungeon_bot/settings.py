38c1205f8057869be1bb9d51c0abbf4451fa8704
Fabrizio Zeni <fabrizio.zeni@gmail.com>
2019-01-17 13:57:31 +0100

    modificati settings

--- dungeon_bot/settings.py
+++ dungeon_bot/settings.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 dungeon_room_amounts= [3,7]
 
-loot_probability_multiplier = 1.3
+loot_probability_multiplier = 0.4
 
-event_cleanse_time = 3 #minutes
+event_cleanse_time = 10 #minutes
 
-inventory_size = 30
+inventory_size = 10
 
-exp_multiplier = 1
+exp_multiplier = 0.1
